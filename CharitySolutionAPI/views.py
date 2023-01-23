8525ef983694a55421f923c287fa97e0273e22c0
YehorVoitenko <egorka.voitenko@gmail.com>
2023-01-23 14:52:17 +0200

    [style] Created login form

--- CharitySolutionAPI/views.py
+++ CharitySolutionAPI/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login
 from CharitySolutionAPI.models import Person
 
 
@@ -16,3 +17,19 @@ def save_user_info(request):
 
 def registration(request):
     return render(request, 'post_info.html')
+
+
+def login_user(request):
+    if request.method['POST']:
+        username = request.POST['username']
+        password = request.POST['password']
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            return redirect('')
+            ...
+        else:
+            # Return an 'invalid login' error message.
+            ...
+    else:
+        return render(request, 'login.html')
