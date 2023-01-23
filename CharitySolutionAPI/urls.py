7b75ce4e9d466c23c0c4bbe34790c859fd6cae5d
YehorVoitenko <egorka.voitenko@gmail.com>
2023-01-23 11:41:03 +0200

    [main] Created MVP with posting and getting data

--- CharitySolutionAPI/urls.py
+++ CharitySolutionAPI/urls.py
@@ -1,8 +1,9 @@
-from django.contrib import admin
 from django.urls import path
 
 from CharitySolutionAPI import views
 
 urlpatterns = [
     path('', views.homepage),
+    path('registration/get_user_info/', views.save_user_info),
+    path('registration/', views.registration),
 ]
--- CharitySolutionAPI/views.py
+++ CharitySolutionAPI/views.py
@@ -1,10 +1,18 @@
-from django.shortcuts import render
-
+from django.shortcuts import render, redirect
 from CharitySolutionAPI.models import Person
 
 
 def homepage(request):
-    tom = Person(name="Test", age=23)
-    tom.save()
-    saved_tom = Person.objects.all()
-    return render(request, 'index.html', context={'context': saved_tom})
+    person_data = Person.objects.all()[::-1]
+    return render(request, 'index.html', context={'context': person_data})
+
+
+def save_user_info(request):
+    user = Person(name=request.POST.get("name"),
+                  age=request.POST.get("age"))
+    user.save()
+    return redirect('/')
+
+
+def registration(request):
+    return render(request, 'post_info.html')
