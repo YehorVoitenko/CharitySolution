eaa00b7b307e66574e84e4a27d1cc4af6bd2d44d
YehorVoitenko <egorka.voitenko@gmail.com>
2023-01-26 17:35:09 +0200

    [account_view] Models uploading

--- CharitySolutionAPI/models.py
+++ CharitySolutionAPI/models.py
@@ -2,5 +2,13 @@ from django.db import models
 
 
 class Person(models.Model):
-    name = models.CharField(max_length=2)
+    name = models.CharField(max_length=255)
     age = models.IntegerField()
+
+
+class UsersPost(models.Model):
+    post_title = models.CharField(max_length=100, null=True)
+    post_text = models.CharField(max_length=2000, null=True)
+    date_created = models.DateTimeField(auto_now_add=True, null=True)
+    file = models.FileField(null=True)
+
