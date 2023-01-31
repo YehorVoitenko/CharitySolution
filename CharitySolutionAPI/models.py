e645efed168b62914e7725d4f2e9736a60e90789
YehorVoitenko <egorka.voitenko@gmail.com>
2023-01-31 18:58:00 +0200

    [account_view] Added blank arg for model 'User Post' in file

--- CharitySolutionAPI/models.py
+++ CharitySolutionAPI/models.py
@@ -2,8 +2,7 @@ from django.db import models
 
 
 class UsersPost(models.Model):
-    post_title = models.CharField(max_length=100, null=True)
-    post_text = models.CharField(max_length=2000, null=True)
-    date_created = models.DateTimeField(auto_now_add=True, null=True)
-    file = models.FileField(null=True)
-
+    post_title = models.CharField(max_length=100)
+    post_text = models.CharField(max_length=2000)
+    date_created = models.DateTimeField(auto_now_add=True)
+    file = models.FileField(blank=True, null=True)
