f8b7850880e5a44e7506c513cc09c523c1dcbd93
YehorVoitenko <egorka.voitenko@gmail.com>
2023-02-01 12:59:08 +0200

    [style] Added names for urls

--- CharitySolutionAPI/urls.py
+++ CharitySolutionAPI/urls.py
@@ -5,10 +5,10 @@ from django.conf.urls.static import static
 from CharitySolutionAPI import views
 
 urlpatterns = [
-    path('get_posts_list/', views.posts_list),
-    path('error/', views.error),
-    path('login_user/', views.login_user),
-    path('logout_user/', views.logout_user),
+    path('get_posts_list/', views.posts_list, name='get_posts_list'),
+    path('error/', views.error, name='error'),
+    path('login_user/', views.login_user, name='login_user'),
+    path('logout_user/', views.logout_user, name='logout_user'),
     path('create_post/', views.create_post, name='create_post'),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
