27c2cc066b2266579d6c73be95a61902262a9922
YehorVoitenko <egorka.voitenko@gmail.com>
2023-02-03 11:34:00 +0200

    [main] Added feature to filter data by organisation_id

--- CharitySolutionAPI/models.py
+++ CharitySolutionAPI/models.py
@@ -1,8 +1,14 @@
 from django.db import models
 
 
+class Organisation(models.Model):
+    organisation_name = models.CharField(max_length=100)
+
+
 class OrganisationPost(models.Model):
+    organisation = models.ForeignKey(Organisation, on_delete=models.CASCADE)
     post_title = models.CharField(max_length=100)
     post_text = models.CharField(max_length=2000)
     date_created = models.DateTimeField(auto_now_add=True)
     file = models.FileField(null=True, blank=True)
+
--- CharitySolutionAPI/views.py
+++ CharitySolutionAPI/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login, logout
 
 from CharitySolutionAPI.forms import OrganisationPostForm
-from CharitySolutionAPI.models import OrganisationPost
+from CharitySolutionAPI.models import OrganisationPost, Organisation
 
 
 def posts_list(request):
@@ -27,6 +27,8 @@ def login_organisation(request):
                                     username=organisation_name,
                                     password=password)
         if organisation is not None:
+            organisation_info = Organisation(organisation_name=organisation_name)
+            organisation_info.save()
             login(request, organisation)
             return redirect('/get_posts_list')
         else:
@@ -44,8 +46,11 @@ def create_post(request):
     if request.user.is_authenticated:
         if request.method == "POST":
             form = OrganisationPostForm(request.POST, request.FILES)
+            organisation = Organisation.objects.get(pk=request.user.id)
             if form.is_valid():
-                form.save()
+                obj = form.save(commit=False)
+                obj.organisation = organisation
+                obj.save()
             return redirect('/get_posts_list')
         form = OrganisationPostForm()
         return render(request, 'create_post.html', {'form': form})
