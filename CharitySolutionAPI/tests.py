5dbfb5d524756add7535952dab5ada24dafceadd
YehorVoitenko <egorka.voitenko@gmail.com>
2023-03-01 12:45:55 +0200

    [tests] Created tests for checking success redirecting and visiting homepage

--- CharitySolutionAPI/tests.py
+++ CharitySolutionAPI/tests.py
@@ -1,3 +1,18 @@
 from django.test import TestCase
 
 # Create your tests here.
+
+
+class TestAPI(TestCase):
+    def test_home_page(self):
+        response = self.client.get('/')
+        self.assertEqual(response.status_code, 200)
+
+    def test_success_post_editing(self):
+        response = self.client.post('/edit_post/1')
+        self.assertEqual(response.status_code, 302)
+
+    def test_success_account_editing(self):
+        response = self.client.post('/edit_organisation_account/1')
+        self.assertEqual(response.status_code, 302)
+
