abc975028ac1e853ce952366ad3ffe07e3c7a0e8
YehorVoitenko <egorka.voitenko@gmail.com>
2023-03-02 14:46:33 +0200

    [tests] Added test for account view

--- CharitySolutionAPI/tests.py
+++ CharitySolutionAPI/tests.py
@@ -1,53 +1,42 @@
 from django.contrib.auth.models import User
 from django.test import TestCase
+
 from CharitySolutionAPI.models import OrganisationPost, Organisation
 
 
 class TestAPI(TestCase):
     def test_homepage(self):
-        response = self.client.get('/')
+        response = self.client.get("/")
         self.assertEqual(response.status_code, 200)
 
     def test_postline_page(self):
-        response = self.client.get('/get_posts_list/')
+        response = self.client.get("/get_posts_list/")
         self.assertEqual(response.status_code, 200)
 
-    # def test_organisation_post_form(self):
-    #     # https://www.valentinog.com/blog/testing-modelform/
-    #     from django.contrib.auth.models import User
-    #     from django.http import HttpRequest
-    #     request = HttpRequest()
-    #     request.POST = {
-    #         "post_title": 'test',
-    #         "post_text": "test",
-    #         "city": "test",
-    #         "help_category": "Another",
-    #         'file': 'test.png'
-    #     }
-    #
-    #     form = OrganisationPostForm(request.POST, request.FILES)
-    #
-    #     self.assertTrue(form.is_valid())
+    def test_account_view(self):
+        organisation_posts = OrganisationPost.objects.filter(
+            organisation=self.test_user.id
+        ).order_by("-date_created")
+
+        response = self.client.post(
+            "/get_account_view/",
+            data={
+                "organisation": self.organisation,
+                "organisation_posts": organisation_posts,
+            },
+        )
+
+        self.assertEqual(response.status_code, 200)
+
+    def test_create_post(self):
+        response = self.client.post("/create_post/")
+        # After success 'create_post' page it have to be redirected to 'get_posts_list'
+        self.assertEqual(response.url, "/get_posts_list")
 
     def test_organisation_creating_in_db(self):
-        Organisation.objects.create(organisation_name='test_organisation',
-                                    organisation_description='test_organisation_description',
-                                    city='test_city', email='email', telegram_nick='telegram_nick',
-                                    instagram_nick='instagram_nick', organisation_site_url='organisation_site_url',
-                                    organisation_logo='test_logo')
-
-        self.assertTrue(Organisation.objects.get(organisation_name='test_organisation'))
+        self.assertTrue(Organisation.objects.get(organisation_name="test_organisation"))
 
     def test_post_creating_in_db(self):
-        Organisation.objects.create(organisation_name='test_organisation')
-        organisation = Organisation.objects.get(organisation_name='test_organisation')
-
-        OrganisationPost.objects.create(organisation=organisation, post_title='test_title', post_text='test_text',
-                                        city='test_city', help_category='Another')
-
-        response = OrganisationPost.objects.get(post_title='test_title')
+        response = OrganisationPost.objects.get(post_title="test_title")
 
         self.assertTrue(response)
-
-        return OrganisationPost.objects.get(post_title='test_title')
-
