4ee541a876951263483deb608059a87870f3fe89
YehorVoitenko <egorka.voitenko@gmail.com>
2023-03-02 11:09:55 +0200

    [code-style] Edited code style by black

--- CharitySolutionAPI/models.py
+++ CharitySolutionAPI/models.py
@@ -1,9 +1,9 @@
 from django.db import models
 
 HELP_CATEGORIES = [
-    ('Humanitarian aid', 'Humanitarian aid'),
-    ('Aid for children', 'Aid for children'),
-    ('Another', 'Another')
+    ("Humanitarian aid", "Humanitarian aid"),
+    ("Aid for children", "Aid for children"),
+    ("Another", "Another"),
 ]
 
 
@@ -15,7 +15,9 @@ class Organisation(models.Model):
     telegram_nick = models.CharField(max_length=100, null=True, blank=True)
     instagram_nick = models.CharField(max_length=100, null=True, blank=True)
     organisation_site_url = models.CharField(max_length=100, null=True, blank=True)
-    organisation_logo = models.FileField(null=True, blank=True, upload_to='organisation_logos')
+    organisation_logo = models.FileField(
+        null=True, blank=True, upload_to="organisation_logos"
+    )
 
     def __str__(self):
         return self.organisation_name
@@ -26,7 +28,9 @@ class OrganisationPost(models.Model):
     post_title = models.CharField(max_length=100)
     post_text = models.CharField(max_length=2000)
     city = models.CharField(max_length=255)
-    help_category = models.CharField(max_length=255, choices=HELP_CATEGORIES, default='Humanitarian aid')
+    help_category = models.CharField(
+        max_length=255, choices=HELP_CATEGORIES, default="Humanitarian aid"
+    )
     date_created = models.DateTimeField(auto_now_add=True)
     date_updated = models.DateTimeField(null=True)
     file = models.FileField(null=True, blank=True)
--- CharitySolutionAPI/tests.py
+++ CharitySolutionAPI/tests.py
@@ -5,14 +5,13 @@ from django.test import TestCase
 
 class TestAPI(TestCase):
     def test_home_page(self):
-        response = self.client.get('/')
+        response = self.client.get("/")
         self.assertEqual(response.status_code, 200)
 
     def test_success_post_editing(self):
-        response = self.client.post('/edit_post/1')
+        response = self.client.post("/edit_post/1")
         self.assertEqual(response.status_code, 302)
 
     def test_success_account_editing(self):
-        response = self.client.post('/edit_organisation_account/1')
+        response = self.client.post("/edit_organisation_account/1")
         self.assertEqual(response.status_code, 302)
-
