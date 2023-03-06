b3c12286af0f36d3033ff593532b70b84a68dd8b
YehorVoitenko <egorka.voitenko@gmail.com>
2023-03-06 23:14:29 +0200

    [code-style] DB settings

--- CharitySolution/settings.py
+++ CharitySolution/settings.py
@@ -77,10 +77,15 @@ WSGI_APPLICATION = 'CharitySolution.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': 'charitysolution.sql',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'CS',
+        'USER': 'postgres',
+        'PASSWORD': '1234567890',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
+
 
 
 # Password validation
