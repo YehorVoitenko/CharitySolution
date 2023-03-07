129dfc7502a912a67bf01dd3af0c27d32a92c10e
YehorVoitenko <egorka.voitenko@gmail.com>
2023-03-07 17:42:37 +0200

    Added project

--- CharitySolution/settings.py
+++ CharitySolution/settings.py
@@ -75,20 +75,13 @@ WSGI_APPLICATION = 'CharitySolution.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-#         'NAME': 'CS',
-#         'USER': 'postgres',
-#         'PASSWORD': '1234567890',
-#         'HOST': 'localhost',
-#         'PORT': '5432',
-#     }
-# }
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': 'sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.environ.get('DB_NAME'),
+        'USER': os.environ.get('DB_USER'),
+        'PASSWORD': os.environ.get('DB_PASS'),
+        'HOST': os.environ.get('DB_HOST'),
     }
 }
 
